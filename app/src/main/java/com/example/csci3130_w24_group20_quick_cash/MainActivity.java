af96122531539ef48ab76dcba791704412ba91cc
Ammar Zain <am494441@dal.ca>
2024-02-07 15:30:19 -0400

    Starting dev branch

---
+++ app/src/main/java/com/example/csci3130_w24_group20_quick_cash/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.csci3130_w24_group20_quick_cash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
