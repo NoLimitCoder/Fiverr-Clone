11ed1cf41cc156a3568a3da8f3150dc889d0a374
Riley Devitt <rl603674@dal.ca>
2024-02-15 13:46:33 -0400

    a

---
+++ app/src/main/java/com/example/csci3130_w24_group20_quick_cash/CredentialValidator.java
@@ -0,0 +1,11 @@
+package com.example.csci3130_w24_group20_quick_cash;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CredentialValidator {
+    protected boolean isEmptyUserName(String userName) {
+        return userName.isEmpty();
+    }
+
+}
