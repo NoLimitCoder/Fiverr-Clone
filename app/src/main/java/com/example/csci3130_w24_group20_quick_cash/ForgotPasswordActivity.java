177ed240151fe77b33eb2ba586725abe5e0e88c6
Riley Devitt <rl603674@dal.ca>
2024-02-15 15:12:38 -0400

    a

--- app/src/main/java/com/example/csci3130_w24_group20_quick_cash/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/csci3130_w24_group20_quick_cash/ForgotPasswordActivity.java
@@ -1,17 +1,48 @@
 package com.example.csci3130_w24_group20_quick_cash;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class forgotPasswordActivity extends AppCompatActivity{
     FirebaseDatabase database = null;
     FirebaseCRUD crud = null;
+    FirebaseAuth auth;
 
-    protected void onCreate
+    @Override
+    protected void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+        setContentView(R.layout.enter_email);
+
+        EditText userEmail = findViewById(R.id.emailEntry);
+        Button send = findViewById(R.id.verifyButton);
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                auth.sendPasswordResetEmail(userEmail.getText().toString()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(forgotPasswordActivity.this, "Check your email to reset password", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
+
+    }
 
     protected void setNewPassword(String email, String password) {
         database = FirebaseDatabase.getInstance(getResources().getString(R.string.FIREBASE_DB_URL));
