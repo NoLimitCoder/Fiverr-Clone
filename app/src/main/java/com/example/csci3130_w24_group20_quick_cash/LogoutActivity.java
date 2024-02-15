e8e86051d20a63e23d3beaa5cf38e6f10197757e
oseibempah <br351912@dal.ca>
2024-02-15 14:50:16 -0400

    Logout Temporary UI page

---
+++ app/src/main/java/com/example/csci3130_w24_group20_quick_cash/LogoutActivity.java
@@ -0,0 +1,20 @@
+package com.example.csci3130_w24_group20_quick_cash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class LogoutActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
