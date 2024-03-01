5bf3274f8a6cfe632ae6a62194fe2216b00619b8
Ammar Zain <am494441@dal.ca>
2024-03-01 19:37:14 -0400

    Activity Registration Text bolded
    Enter Email Screen redone
    
    enter email method now closes keyboard onclick

--- app/src/main/java/com/example/csci3130_w24_group20_quick_cash/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/csci3130_w24_group20_quick_cash/ForgotPasswordActivity.java
@@ -1,7 +1,10 @@
 package com.example.csci3130_w24_group20_quick_cash;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -22,6 +25,28 @@ public class ForgotPasswordActivity extends AppCompatActivity {
         setContentView(R.layout.enter_email);
         mAuth = FirebaseAuthSingleton.getInstance();
         this.setupSendButton();
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        View view = getCurrentFocus();
+        boolean ret = super.dispatchTouchEvent(event);
+
+        if (view instanceof EditText) {
+            View w = getCurrentFocus();
+            int scrcoords[] = new int[2];
+            w.getLocationOnScreen(scrcoords);
+            float x = event.getRawX() + w.getLeft() - scrcoords[0];
+            float y = event.getRawY() + w.getTop() - scrcoords[1];
+
+            if (event.getAction() == MotionEvent.ACTION_UP
+                    && (x < w.getLeft() || x >= w.getRight()
+                    || y < w.getTop() || y > w.getBottom()) ) {
+                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(getWindow().getCurrentFocus().getWindowToken(), 0);
+            }
+        }
+        return ret;
     }
 
     protected void setupSendButton() {
