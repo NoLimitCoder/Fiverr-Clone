6560a78ce55a5a1eef0a324bbaee830f256e3802
Sebastian Dionicio <sdionicio@dal.ca>
2024-04-07 20:13:46 -0300

    Tests for JobOfferAdapterTest added, UserJobs relatedot adapter tests solved in Pair Programming with Ammar

--- app/src/test/java/com/example/csci3130_w24_group20_quick_cash/JobOfferAdapterTest.java
+++ app/src/test/java/com/example/csci3130_w24_group20_quick_cash/JobOfferAdapterTest.java
@@ -1,2 +1,37 @@
-package com.example.csci3130_w24_group20_quick_cash;public class JobOfferAdapterTest {
+package com.example.csci3130_w24_group20_quick_cash;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class JobOfferAdapterTest {
+
+    private List<JobOffer> jobOffers;
+
+    @Before
+    public void setUp() {
+        jobOffers = new ArrayList<>();
+        jobOffers.add(new JobOffer());
+        jobOffers.add(new JobOffer());
+    }
+
+    @Test
+    public void testAdapterItemCount() {
+        JobOfferAdapter adapter = new JobOfferAdapter(jobOffers, null);
+        assertEquals(jobOffers.size(), adapter.getItemCount());
+    }
+
+
+    @Test
+    public void testAdapterMethodsCount() {
+        JobOfferAdapter adapterMethod = new JobOfferAdapter(jobOffers, null);
+        assertEquals(jobOffers.size(), adapterMethod.getItemCount());
+        assertNotNull(adapterMethod);
+    }
 }
+
